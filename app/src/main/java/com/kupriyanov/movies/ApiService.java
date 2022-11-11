8761b1966964f2e9a5c468a8a9ebd8bb67827aa7
AltF4ntom <AltF4ntom@yandex.ru>
2022-11-11 19:51:47 +0300

    RecyclerView, MoviesAdapter, MainViewModel, circle(green, orange and red)

--- app/src/main/java/com/kupriyanov/movies/ApiService.java
+++ app/src/main/java/com/kupriyanov/movies/ApiService.java
@@ -2,9 +2,10 @@ package com.kupriyanov.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=F47PHVG-CMV43MT-P6ET91H-6R2YB3Q&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=5")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?token=F47PHVG-CMV43MT-P6ET91H-6R2YB3Q&field=rating.kp&search=4-7&sortField=votes.kp&sortType=-1&limit=30")
+    Single<MovieResponse> loadMovies(@Query("page")int page);
 }
--- app/src/main/java/com/kupriyanov/movies/MainActivity.java
+++ app/src/main/java/com/kupriyanov/movies/MainActivity.java
@@ -1,9 +1,17 @@
 package com.kupriyanov.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.functions.Consumer;
@@ -11,26 +19,44 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private MainViewModel viewModel;
+    private MoviesAdapter moviesAdapter;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBarLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ApiFactory.apiService.loadMovies()
-                .observeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MovieResponse>() {
-                    @Override
-                    public void accept(MovieResponse movieResponse) throws Throwable {
-                        Log.d(TAG, movieResponse.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.d(TAG, throwable.toString());
-                    }
-                });
+        progressBarLoading = findViewById(R.id.progressBarLoading);
+        recyclerView = findViewById(R.id.recyclerViewMovies);
+        moviesAdapter = new MoviesAdapter();
+        recyclerView.setAdapter(moviesAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                moviesAdapter.setMovies(movies);
+            }
+        });
+        moviesAdapter.setOnReachEndListener(new MoviesAdapter.OnReachEndListener() {
+            @Override
+            public void onReachEnd() {
+                viewModel.loadMovies();
+            }
+        });
+        viewModel.getIsLoading().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isLoading) {
+                if (isLoading){
+                    progressBarLoading.setVisibility(View.VISIBLE);
+                } else {
+                    progressBarLoading.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
